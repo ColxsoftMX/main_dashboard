de4d909a1cdebe75f7f75a188c2bcedc7eefdd9a
PaAdRaAg <pramos12@ucol.mx>
2024-02-13 23:36:55 -0600

    texto en inglés

---
+++ src/assets/login/translations/en.ts
@@ -0,0 +1,15 @@
+const translations = {
+  t1: 'Welcome',
+  t2: 'Sign in to your account',
+  t3: 'Email',
+  t4: 'Enter your email',
+  t4W: 'Please enter a email!',
+  t5: 'Password',
+  t5w: 'Please enter a password!',
+  t6: 'Enter your password',
+  t7: 'Remember me',
+  t8: 'Forgot your password?',
+  t9: 'Sign in',
+};
+
+export default translations;
