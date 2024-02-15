f05bdc21d443ccfcb8f2e2b27f8aee7687df17de
PaAdRaAg <pramos12@ucol.mx>
2024-02-14 20:02:25 -0600

    modificación para siempre mostrar el layout y el footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Layout } from 'antd'
 import './App.css'
 import LoginView from './components/login/LoginView'
+import Footer from './components/global/Footer';
+
+const { Content } = Layout;
 
 function App() {
 
   return (
     <>
-      <LoginView />
+      <Layout style={{ background: '#E3E3E3' }}>
+        <Content style={{ margin: '60px 40px 40px' }}>
+          <LoginView />
+        </Content>
+        <Footer />
+      </Layout>
     </>
   )
 }
