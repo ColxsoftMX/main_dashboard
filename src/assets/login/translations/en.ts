27699239eb037d45d952634b13131a34bdee5e7b
PaAdRaAg <pramos12@ucol.mx>
2024-02-16 00:13:01 -0600

    adición de mensaje para modal de error en caso de un email no válido

--- src/assets/login/translations/en.ts
+++ src/assets/login/translations/en.ts
@@ -4,12 +4,14 @@ const translations = {
   t3: 'Email',
   t4: 'Enter your email',
   t4W: 'Please enter a email!',
+  t4W2: 'Please enter a valid email!',
   t5: 'Password',
   t5w: 'Please enter a password!',
   t6: 'Enter your password',
   t7: 'Remember me',
   t8: 'Forgot your password?',
   t9: 'Sign in',
+  err: 'There was a problem signing in',
 };
 
 export default translations;
--- src/assets/login/translations/es.ts
+++ src/assets/login/translations/es.ts
@@ -4,12 +4,14 @@ const translations = {
     t3: 'Correo electrónico',
     t4: 'Ingrese su correo electrónico',
     t4W: '¡Por favor ingresa un correo electrónico!',
+    t4W2: '¡Por favor ingresa un correo electrónico válido!',
     t5: 'Contraseña',
     t5w: '¡Por favor ingresa una contraseña!',
     t6: 'Ingrese su contraseña',
     t7: 'Recuérdame',
     t8: '¿Olvidaste tu contraseña?',
     t9: 'Iniciar sesión',
+    err: 'Hubo un problema al iniciar sesión',
 };
 
 export default translations;
