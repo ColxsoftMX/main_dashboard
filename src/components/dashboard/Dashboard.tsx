7071480ed3e706b973fda356faba2151fa87d8a9
PaAdRaAg <pramos12@ucol.mx>
2024-02-16 01:04:13 -0600

    generación de componente de prueba para dashboard

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import { Row } from "antd";
+
+const Dashboard: React.FC = () => {
+
+    return (
+        <div
+            style={{
+                height: '100%',
+                background: '#fff',
+                borderRadius: 8,
+            }}
+        >
+            <Row>
+                <h1>Dashboard</h1>
+            </Row>
+        </div>
+    );
+}
+
+export default Dashboard;
