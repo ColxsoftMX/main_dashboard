b5166c16cdf1faab5442bc474fe2d6e35eafe65f
PaAdRaAg <pramos12@ucol.mx>
2024-02-16 01:06:41 -0600

    cambio de turas de archivos y componente de traducción, adición para alt de imagen de login

---
+++ src/components/login/translations/es.ts
@@ -0,0 +1,18 @@
+const translations = {
+    t1: 'Bienvenido',
+    t2: 'Inicie sesión en su cuenta',
+    t3: 'Correo electrónico',
+    t4: 'Ingrese su correo electrónico',
+    t4W: '¡Por favor ingresa un correo electrónico!',
+    t4W2: '¡Por favor ingresa un correo electrónico válido!',
+    t5: 'Contraseña',
+    t5w: '¡Por favor ingresa una contraseña!',
+    t6: 'Ingrese su contraseña',
+    t7: 'Recuérdame',
+    t8: '¿Olvidaste tu contraseña?',
+    t9: 'Iniciar sesión',
+    err: 'Hubo un problema al iniciar sesión',
+    img: 'Personas en una biblioteca hablando juntas'
+};
+
+export default translations;
