ae9f6568fa35d5ab2cafafdedd557c1b90229c46
PaAdRaAg <pramos12@ucol.mx>
2024-02-16 21:35:54 -0600

    adecuaciones para uso de params modificados de traducciones, implementación de función para traducción de header

---
+++ src/utils/translations/i18n.ts
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react';
+
+type Languages = 'es' | 'en';
+
+let currentLanguage: Languages = 'es';
+//////////////////////////////////////////////////////////////////////////////
+import { esTranslationsLogin } from './es';
+import { enTranslationsLogin } from './en';
+
+const languagesLogin: Record<Languages, Record<string, string>> = {
+  es: esTranslationsLogin,
+  en: enTranslationsLogin,
+};
+
+const useLanguageLogin = () => {
+  const [language, setLanguageState] = useState<Languages>(currentLanguage);
+
+  useEffect(() => {
+    localStorage.setItem('language', currentLanguage);
+  }, []);
+
+  const setLanguage = (lang: Languages) => {
+    localStorage.removeItem('language');
+
+    if (languagesLogin[lang]) {
+      currentLanguage = lang;
+      setLanguageState(lang);
+      localStorage.setItem('language', lang);
+    }
+  };
+
+  const t = (key: string) => {
+    return languagesLogin[currentLanguage][key] || key;
+  };
+
+  return { language, setLanguage, t };
+};
+
+//////////////////////////////////////////////////////////////////////////////
+import { esTranslationsHeader } from './es';
+import { enTranslationsHeader } from './en';
+
+const languagesHeader: Record<Languages, Record<string, string>> = {
+  es: esTranslationsHeader,
+  en: enTranslationsHeader,
+};
+
+const LocalstorageLanguage = localStorage.getItem('language')
+
+console.log(LocalstorageLanguage)
+
+const useLanguageHeader = () => {
+  const [language, setLanguageState] = useState<Languages>(LocalstorageLanguage as Languages);
+
+  const setLanguage = (lang: Languages) => {
+    localStorage.removeItem('language');
+
+    if (languagesHeader[lang]) {
+      currentLanguage = lang;
+      setLanguageState(lang);
+      localStorage.setItem('language', lang);
+    }
+  };
+
+  const t = (key: string) => {
+    return languagesHeader[currentLanguage][key] || key;
+  };
+
+  return { language, setLanguage, t };
+};
+
+export { useLanguageLogin, useLanguageHeader };
