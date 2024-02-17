b26f30acf9bc49ece8e571fb6a163e3a83091a1a
PaAdRaAg <pramos12@ucol.mx>
2024-02-17 00:17:30 -0600

    componente para generar las tarjetas de los servicios a partir de un array

---
+++ src/components/dashboard/Cards.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, Row, Col } from 'antd';
+
+const { Meta } = Card;
+
+interface DashboardCardProps {
+  nombre: string;
+  imagen: string;
+  path: string;
+}
+
+const DashboardCard: React.FC<DashboardCardProps> = ({ nombre, imagen, path }) => (
+  <Col xs={24} sm={12} md={8} lg={6}>
+    <Card
+      hoverable
+      cover={<img alt={nombre} src={imagen} style={{ height: '150px', objectFit: 'cover' }} />}
+    >
+      <Meta title={nombre} description={<a href={path}>Ir al dashboard</a>} />
+    </Card>
+  </Col>
+);
+
+interface DashboardCardsProps {
+  data: DashboardCardProps[];
+}
+
+const DashboardCards: React.FC<DashboardCardsProps> = ({ data }) => (
+  <Row gutter={[16, 16]}>
+    {data.map((item, index) => (
+      <DashboardCard key={index} {...item} />
+    ))}
+  </Row>
+);
+
+export default DashboardCards;
