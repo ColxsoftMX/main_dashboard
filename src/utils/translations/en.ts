0b530366b3e406194220a4a791569773be3d2a7b
PaAdRaAg <pramos12@ucol.mx>
2024-02-16 21:34:32 -0600

    cambios en params de traducción para el login y adición de param de traducción para el header

---
+++ src/utils/translations/en.ts
@@ -0,0 +1,24 @@
+export const enTranslationsLogin = {
+  L1: 'Welcome',
+  L2: 'Sign in to your account',
+  L3: 'Email',
+  L4: 'Enter your email',
+  L4W: 'Please enter a email!',
+  L4W2: 'Please enter a valid email!',
+  L5: 'Password',
+  L5w: 'Please enter a password!',
+  L6: 'Enter your password',
+  L7: 'Remember me',
+  L8: 'Forgot your password?',
+  L9: 'Sign in',
+  err: 'There was a problem signing in',
+  img: 'Pople in a library talking together'
+};
+
+export const enTranslationsHeader = {
+  H1: 'Colxsoft',
+  H2: 'Home',
+  H3: 'Services',
+  H4: 'Users',
+  H5: 'Log out'
+};
