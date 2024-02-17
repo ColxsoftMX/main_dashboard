6271e95ba65d07409199bb1355ea66bdf749ad4a
PaAdRaAg <pramos12@ucol.mx>
2024-02-17 00:34:53 -0600

    implementación de componente dashboardcards con datos de prueba

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,6 +1,24 @@
 import { Row } from "antd";
+import HeaderView from "../global/header/Header";
+import { useLanguageDashboard } from "../../utils/translations/i18n.js";
+import DashboardCards from "./Cards.js";
+import dashboardImage from './prueba.jpg';
 
 const Dashboard: React.FC = () => {
+    const { t } = useLanguageDashboard();
+
+    const data = [
+        {
+            nombre: 'Dashboard 1',
+            imagen: dashboardImage,
+            path: '/app/invitaciones/',
+        },
+        {
+            nombre: 'Dashboard 2',
+            imagen: dashboardImage,
+            path: '/app/dashboard/',
+        },
+    ];
 
     return (
         <div
@@ -10,11 +28,17 @@ const Dashboard: React.FC = () => {
                 borderRadius: 8,
             }}
         >
-            <Row>
-                <h1>Dashboard</h1>
+            <HeaderView />
+            <Row style={{ margin: '0 0 0 20px' }}>
+                <h1>{t('D1')}</h1>
             </Row>
+
+            {data.length > 0 ? (
+                <DashboardCards data={data} />
+                ) : null}
         </div>
     );
+
 }
 
 export default Dashboard;
