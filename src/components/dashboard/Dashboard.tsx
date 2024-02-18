d0b53c02ebbac33b1892e874402fd776c10700d7
PaAdRaAg <pramos12@ucol.mx>
2024-02-17 20:12:40 -0600

    adición de param a objeto de generación de tarjetas, implementación de multiples funciones de traducciín, adición de string con función de traducción para mandar el valor por defecto seleccionado en el menú del header

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,53 +1,60 @@
 import { Row } from "antd";
 import HeaderView from "../global/Header.js";
-import { useLanguageDashboard } from "../../utils/translations/i18n.js";
+import { useLanguageDashboard, useLanguageHeader } from "../../utils/translations/i18n.js";
 import DashboardCards from "./Cards.js";
 import dashboardImage from './prueba.jpg';
+import mesa from './mesas.jpg';
+import inventario from './inventario.jpg';
+import invitaciones from './invitaciones.jpg';
 
 const Dashboard: React.FC = () => {
-    const { t } = useLanguageDashboard();
+    const { t: tHeader } = useLanguageHeader();
+    const { t: tDashboard } = useLanguageDashboard();
 
     const data = [
         {
             nombre: 'Dashboard 1',
             imagen: dashboardImage,
             path: '/app/invitaciones/',
+            descripcion: 'Dashboard de prueba',
+        },
+        {
+            nombre: 'Gestión de mesas',
+            imagen: mesa,
+            path: '/app/invitaciones/',
+            descripcion: 'Gestión de mesas',
         },
         {
             nombre: 'Dashboard 2',
-            imagen: dashboardImage,
-            path: '/app/dashboard/',
-        },
-        {
-            nombre: 'Dashboard 1',
             imagen: dashboardImage,
             path: '/app/invitaciones/',
+            descripcion: 'Dashboard de prueba',
         },
         {
-            nombre: 'Dashboard 2',
-            imagen: dashboardImage,
-            path: '/app/dashboard/',
+            nombre: 'Inventario',
+            imagen: inventario,
+            path: '/app/invitaciones/',
+            descripcion: 'Inventario de productos',
         },
         {
-            nombre: 'Dashboard 1',
+            nombre: 'Dashboard 3',
             imagen: dashboardImage,
             path: '/app/invitaciones/',
+            descripcion: 'Dashboard de prueba',
         },
         {
-            nombre: 'Dashboard 2',
-            imagen: dashboardImage,
-            path: '/app/dashboard/',
+            nombre: 'Invitaciones',
+            imagen: invitaciones,
+            path: '/app/invitaciones/',
+            descripcion: 'Gestión de invitaciones',
         },
         {
-            nombre: 'Dashboard 1',
+            nombre: 'Dashboard 4',
             imagen: dashboardImage,
             path: '/app/invitaciones/',
+            descripcion: 'Dashboard de prueba',
         },
-        {
-            nombre: 'Dashboard 2',
-            imagen: dashboardImage,
-            path: '/app/dashboard/',
-        },
+        
     ];
 
     return (
@@ -58,9 +65,9 @@ const Dashboard: React.FC = () => {
                 borderRadius: 8,
             }}
         >
-            <HeaderView />
-            <Row style={{ margin: '0 0 0 30px' }}>
-                <h1>{t('D1')}</h1>
+            <HeaderView activeTab={tHeader('H2')} />
+            <Row style={{ margin: '0 0 0 30px', height: '60px' }}>
+                <h1 style={{ margin: '10px 20px' }}>{tDashboard('D1')}</h1>
             </Row>
 
             {data.length > 0 ? (
