3fa249db1c3b029588aba8a59db8e18959828252
PaAdRaAg <pramos12@ucol.mx>
2024-02-17 20:19:15 -0600

    modificación a componente para optimizar la imple,mentación de funciones y su uso

--- src/utils/translations/i18n.ts
+++ src/utils/translations/i18n.ts
@@ -3,16 +3,8 @@ import { useEffect, useState } from 'react';
 type Languages = 'es' | 'en';
 
 let currentLanguage: Languages = 'es';
-//////////////////////////////////////////////////////////////////////////////
-import { esTranslationsLogin } from './es';
-import { enTranslationsLogin } from './en';
 
-const languagesLogin: Record<Languages, Record<string, string>> = {
-  es: esTranslationsLogin,
-  en: enTranslationsLogin,
-};
-
-const useLanguageLogin = () => {
+const useLanguage = (translations: Record<Languages, Record<string, string>>) => {
   const [language, setLanguageState] = useState<Languages>(currentLanguage);
 
   useEffect(() => {
@@ -22,7 +14,7 @@ const useLanguageLogin = () => {
   const setLanguage = (lang: Languages) => {
     localStorage.removeItem('language');
 
-    if (languagesLogin[lang]) {
+    if (translations[lang]) {
       currentLanguage = lang;
       setLanguageState(lang);
       localStorage.setItem('language', lang);
@@ -30,72 +22,32 @@ const useLanguageLogin = () => {
   };
 
   const t = (key: string) => {
-    return languagesLogin[currentLanguage][key] || key;
+    return translations[currentLanguage][key] || key;
   };
 
   return { language, setLanguage, t };
 };
 
-//////////////////////////////////////////////////////////////////////////////
-import { esTranslationsHeader } from './es';
-import { enTranslationsHeader } from './en';
+export { useLanguage };
+
+import { esTranslationsLogin, esTranslationsHeader, esTranslationsDashboard } from './es';
+import { enTranslationsLogin, enTranslationsHeader, enTranslationsDashboard } from './en';
+
+const languagesLogin: Record<Languages, Record<string, string>> = {
+  es: esTranslationsLogin,
+  en: enTranslationsLogin,
+};
 
 const languagesHeader: Record<Languages, Record<string, string>> = {
   es: esTranslationsHeader,
   en: enTranslationsHeader,
 };
 
-const LocalstorageLanguage = localStorage.getItem('language')
-
-console.log(LocalstorageLanguage)
-
-const useLanguageHeader = () => {
-  const [language, setLanguageState] = useState<Languages>(LocalstorageLanguage as Languages);
-
-  const setLanguage = (lang: Languages) => {
-    localStorage.removeItem('language');
-
-    if (languagesHeader[lang]) {
-      currentLanguage = lang;
-      setLanguageState(lang);
-      localStorage.setItem('language', lang);
-    }
-  };
-
-  const t = (key: string) => {
-    return languagesHeader[currentLanguage][key] || key;
-  };
-
-  return { language, setLanguage, t };
-};
-
-//////////////////////////////////////////////////////////////////////////////
-import { esTranslationsDashboard } from './es';
-import { enTranslationsDashboard } from './en';
-
 const languagesDashboard: Record<Languages, Record<string, string>> = {
   es: esTranslationsDashboard,
   en: enTranslationsDashboard,
 };
 
-const useLanguageDashboard = () => {
-  const [language, setLanguageState] = useState<Languages>(currentLanguage);
-
-  const setLanguage = (lang: Languages) => {
-    localStorage.removeItem('language');
-
-    if (languagesDashboard[lang]) {
-      currentLanguage = lang;
-      setLanguageState(lang);
-      localStorage.setItem('language', lang);
-    }
-  };
-
-  const t = (key: string) => {
-    return languagesDashboard[currentLanguage][key] || key;
-  };
-
-  return { language, setLanguage, t };
-};
-
-export { useLanguageLogin, useLanguageHeader, useLanguageDashboard };
+export const useLanguageLogin = () => useLanguage(languagesLogin);
+export const useLanguageHeader = () => useLanguage(languagesHeader);
+export const useLanguageDashboard = () => useLanguage(languagesDashboard);
