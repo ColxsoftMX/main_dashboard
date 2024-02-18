95981349c18d5e14096975dec6b085993af17651
PaAdRaAg <pramos12@ucol.mx>
2024-02-17 20:18:42 -0600

    modificaciones para nuevo uso de funciones de traducción, acomodo a estilos, eliminación de apartados sin usar y otras cosas

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,60 +1,39 @@
-import { Layout, Menu, /*Select*/ } from 'antd';
+import { Layout, Menu } from 'antd';
 import { useLanguageHeader } from '../../utils/translations/i18n';
-// import { Option } from 'antd/es/mentions';
 import { useNavigate } from 'react-router-dom';
-// import { useEffect } from 'react';
 
 const { Header } = Layout;
 
-const HeaderView: React.FC = () => {
-    // const lang = localStorage.getItem('language');
+interface HeaderViewProps {
+  activeTab?: string;
+}
 
-    // useEffect(() => {
-    //     if (lang) {
-    //         setLanguage(lang as 'es' | 'en');
-    //     }
-    // }, []);
+const HeaderView: React.FC<HeaderViewProps> = ({ activeTab }) => {
+  const { t } = useLanguageHeader();
+  const navigate = useNavigate();
 
-    const { /*language, setLanguage,*/ t } = useLanguageHeader();
+  return (
+    <Header style={{ padding: 0, background: 'white', borderRadius: 8 }}>
+      <div style={{ display: 'flex', background: 'white', borderRadius: 8, justifyContent: 'space-between' }}>
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={[activeTab || '']} style={{ width: '100%', height: '50px', borderRadius: '8px 0 0 0', boxShadow: '0px 5px 3px -5px #DFDFDF'}} disabled>
+          <Menu.Item key={t('H1')} style={{ textAlign: 'left', marginLeft: '20px' }}>
+            <h1 style={{ color: 'black', margin: '0', padding: 0 }}>{t('H1')}</h1>
+          </Menu.Item>
 
-    // const handleLanguageChange = (value: string) => {
-    //     setLanguage(value as 'es' | 'en');
-    // };
+        </Menu>
 
-    const navigate = useNavigate();
-
-    return (
-        <Header style={{ padding: 0, background: 'white', borderRadius: 8 }}>
-            <div style={{ display: 'flex', background: 'white', borderRadius: 8 }}>
-                <Menu theme="light" mode="horizontal" style={{ width: '100%', height: '50px', borderRadius: 8 }} >
-                    <div style={{ width: '50%', borderRadius: 8 }}>
-                        <h1 style={{ color: 'black', margin: '0 0 0 20px', padding: 0 }}>{t('H1')}</h1>
-                    </div>
-
-                    <div style={{ width: '50%' }}>
-                        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['1']} style={{ ...(window.innerWidth >= 1005 ? { width: '43%' } : {width: '40%'}) , height: '50px', borderRadius: '0 8px 0 0', position: 'absolute', justifyContent: 'end' }} >
-                            <Menu.Item key="1">{t('H2')}</Menu.Item>
-                            <Menu.Item key="2">{t('H3')}</Menu.Item>
-                            <Menu.Item key="3" onClick={() => navigate('/')}>
-                                {t('H4')}
-                            </Menu.Item>
-                        </Menu>
-                    </div>
-
-                    {/* <div style={{ width: '13%' }}>
-                        <Select
-                        defaultValue={language}
-                        style={{ width: 90, margin: '0' }}
-                        onChange={handleLanguageChange}
-                        >
-                        <Option value="es">Español</Option>
-                        <Option value="en">English</Option>
-                        </Select>
-                    </div> */}
-                </Menu>
-            </div>
-        </Header>
-    );
+        <Menu theme="light" mode="horizontal" defaultSelectedKeys={[activeTab || '']} style={{ width: '25%', height: '50px', borderRadius: '0 8px 0 0', boxShadow: '0px 5px 3px -5px #DFDFDF' }}>
+        <Menu.Item key={t('H2')} onClick={() => navigate('/dashboard')}>
+            {t('H2')}
+          </Menu.Item>
+          <Menu.Item key={t('H3')}>{t('H3')}</Menu.Item>
+          <Menu.Item key={t('H4')} onClick={() => navigate('/')}>
+            {t('H4')}
+          </Menu.Item>
+</Menu>
+      </div>
+    </Header>
+  );
 };
 
 export default HeaderView;
