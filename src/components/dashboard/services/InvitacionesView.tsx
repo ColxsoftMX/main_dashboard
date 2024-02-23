e731516dd81604969c9512eeb72d514aaf5a830a
PaAdRaAg <pramos12@ucol.mx>
2024-02-22 21:06:07 -0600

    reubicación de invitaciones

---
+++ src/components/dashboard/services/InvitacionesView.tsx
@@ -0,0 +1,22 @@
+import { Row } from "antd";
+import HeaderView from "../../global/Header";
+
+const InvitacionesView: React.FC = () => {
+
+    return (
+        <div
+            style={{
+                height: '100%',
+                background: '#fff',
+                borderRadius: 8,
+            }}
+        >
+            <HeaderView />
+            <Row style={{ margin: '0 0 0 20px' }}>
+                <h1>Invitaciones</h1>
+            </Row>
+        </div>
+    );
+}
+
+export default InvitacionesView;
