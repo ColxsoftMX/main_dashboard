28bebb2989bb0320ced7736fa206c0b4d3d7c862
PaAdRaAg <pramos12@ucol.mx>
2024-02-22 22:24:21 -0600

    corrección a invitaciones view

---
+++ src/components/dashboard/services/invitations/InvitationsView.tsx
@@ -0,0 +1,25 @@
+import { Row } from "antd";
+import HeaderView from "../../../global/Header";
+import { useLanguageInvitations } from "../../../../utils/translations/i18n";
+
+const InvitationsView: React.FC = () => {
+
+    const { t } = useLanguageInvitations();
+
+    return (
+        <div
+            style={{
+                height: '100%',
+                background: '#fff',
+                borderRadius: 8,
+            }}
+        >
+            <HeaderView />
+            <Row style={{ margin: '0 0 0 20px' }}>
+                <h1 style={{ margin: '10px 20px' }}>{t('I1')}</h1>
+            </Row>
+        </div>
+    );
+}
+
+export default InvitationsView;
