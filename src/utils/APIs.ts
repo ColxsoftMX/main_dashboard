5d0279bec174d480a0b0566ec2614806de0f3f18
PaAdRaAg <pramos12@ucol.mx>
2024-03-10 20:01:26 -0600

    adicicón de token para validación de token

--- src/utils/APIs.ts
+++ src/utils/APIs.ts
@@ -5,3 +5,7 @@ const host = 'https://qa.colxsoft.com/';
 // const host = 'https://colxsoft.com/';
 
 export const LoginApi = `${host}usuarios/api/users.php`;
+
+// export const ModulesApi = `${host}
+
+export const ValidateTokenApi = `${host}/usuarios/api/permisos.php`;
